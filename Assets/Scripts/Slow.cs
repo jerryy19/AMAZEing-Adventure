e2663fc4a31296e7bc2cb6e97801c39ce35d6a83
unknown <46431639+jerryy19@users.noreply.github.com>
2022-12-14 20:46:26 -0500

    skeleton code for tutorial, speed, slow, mystery scripts

---
+++ Assets/Scripts/Slow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slow : MonoBehaviour
+{
+    GameObject level;
+    Collider c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = GameObject.Find("Level");
+        c = gameObject.AddComponent<Collider>();
+        c.isTrigger = true;
+
+        Bounds bounds = GetComponent<Collider>().bounds;
+        bounds.size *= 0.2f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // when player enter slow field, it slows the player down for some time
+        // get play from level
+    }
+}
