61034ec2a712d4776652fcb06548aa225af49531
liambrandwein <61393448+liambrandwein@users.noreply.github.com>
2022-12-16 16:56:50 -0500

    Adding tracking rotation, sounds
    Adding tracking rotation to bananamen (tracking position of bigvegas when they can see him). Also put some sounds in a folder for use later.

--- Assets/Enemy/BananaMan.cs
+++ Assets/Enemy/BananaMan.cs
@@ -11,9 +11,14 @@ public class BananaMan : MonoBehaviour
     public Transform hand;
     public Animator animator;
 
+    private GameObject player;
+    private Vector3 direction_to_player;
+    
     // Start is called before the first frame update
     void Start()
     {
+        direction_to_player = new Vector3(0, 0, 0);
+        player = GameObject.Find("BigVegas(Clone)");
         level = GameObject.Find("Level");
         animator = GetComponent<Animator>();
         // Adding throw event to throw animation
@@ -24,6 +29,19 @@ public class BananaMan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 player_centroid = player.GetComponent<CapsuleCollider>().bounds.center;
+        Vector3 bananaman_centroid = GetComponent<CapsuleCollider>().bounds.center;
+        direction_to_player = (player_centroid - bananaman_centroid);
+        direction_to_player.Normalize();
+        RaycastHit hit;
+        if (Physics.Raycast(bananaman_centroid, direction_to_player, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject == player)
+            {
+                float angle_to_rotate = Mathf.Rad2Deg * Mathf.Atan2(direction_to_player.x, direction_to_player.z);
+                transform.eulerAngles = new Vector3(0.0f, angle_to_rotate, 0.0f);
+            }
+        }
         if (Input.GetKey(KeyCode.Space))
         {
             animator.SetBool("isIdle", false);
